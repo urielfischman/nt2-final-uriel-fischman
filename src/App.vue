2fb94b82764788daf82cfcb8832972504da50f2b
Uriel1621 <uriel.fischman@gmail.com>
2022-07-12 20:44:23 -0300

    Final - Uriel Fischman

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <Ejercicio/>
+    <br>
+    <table>
+      <tr>
+        <th>Numero de pregunta</th>
+        <th>Respuesta</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>C</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>D</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>D</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import Ejercicio from './components/Ejercicio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ejercicio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+
+th, td {
+  padding: 5px;
+}
+</style>
---
+++ src/components/Ejercicio.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <section class="src-components-ejercicio">
+    
+    <label for="color"> Color: </label>
+    <input name="color" type="text" v-model.trim="color">
+    <label for="fondo"> Fondo: </label>
+    <input name="fondo" type="text" v-model.trim="fondo">
+    <label for="tamanio"> Tamaño: </label>
+    <input name="tamanio" type="text" v-model.trim="tamanio">
+    <hr>
+    <label for="texto"> Texto: </label>
+    <input name="texto" type="text" v-model.trim="texto" :style="getStyle">
+    <br>
+    <p v-if="texto.length">Cantidad de palabras: {{getCantidadPalabras}}</p>
+
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-ejercicio',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        texto : "",
+        color: "",
+        fondo: "",
+        tamanio: ""
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      getStyle(){
+        return {color : this.color, "background-color" : this.fondo, "font-size" : this.tamanio}
+      },
+      getCantidadPalabras(){
+        let cant
+        if(this.texto.length){
+          cant = this.texto.split(" ").length
+        }else{
+          cant = 0
+        }
+        return cant
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-ejercicio {
+
+  }
+
+</style>
